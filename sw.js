727e940be830941b9e0df51d3a2334df3b0e4b5b
tatsuro2731 <musze78e@gmail.com>
2026-07-13 08:05:21 +0900

    Use delivery scooter guide icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="ubereats-progress-v19";
-const ASSETS=["./","index.html","compact.html","manifest.webmanifest","apple-touch-icon.png","assets/favicon-32.png","assets/icon-192.png","assets/icon-512.png"];
+const CACHE="ubereats-progress-v20";
+const ASSETS=["./","index.html","compact.html","manifest.webmanifest","apple-touch-icon.png","assets/favicon-32.png","assets/icon-192.png","assets/icon-512.png","assets/delivery-scooter.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{if(res&&res.ok&&res.type==="basic"){let cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp))}return res}).catch(()=>caches.match("index.html",{ignoreSearch:true}))))});
