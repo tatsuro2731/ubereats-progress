9309ae56f2edad1aff552e68fedf63dbfcf57734
tatsuro2731 <musze78e@gmail.com>
2026-07-13 09:52:50 +0900

    Add drag reorder for metric cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ubereats-progress-v22";
+const CACHE="ubereats-progress-v23";
 const ASSETS=["./","index.html","compact.html","manifest.webmanifest","apple-touch-icon.png","assets/favicon-32.png","assets/icon-192.png","assets/icon-512.png","assets/delivery-scooter.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
